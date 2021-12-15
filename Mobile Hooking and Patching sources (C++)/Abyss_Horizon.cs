2dc227dfe8f356cbf4c1b15d5f6473981ed34d68
YasKashije <94450913+YasKashije@users.noreply.github.com>
2021-12-15 01:25:33 +0100

    Create Abyss_Horizon.cs

---
+++ Mobile Hooking and Patching sources (C++)/Abyss_Horizon.cs
@@ -0,0 +1,17 @@
+//https://play.google.com/store/apps/details?id=com.abysshorizonlt
+
+
+
+public class DamageCalculate
+
+	public static int CaculateCannonDamage(BoatUnit srcBoat, BoatUnit tagBoat, BoatGroup srcGroup, DamageBuff dmgBuff, bool isCritical) { }
+
+	public static int CaculateFlightDamage(BoatUnit srcBoat, BoatUnit tagBoat, BoatGroup srcGroup, DamageBuff dmgBuff, bool isCritical) { }
+
+	public static int CaculateTorpedoDamage(BoatUnit srcBoat, BoatUnit tagBoat, BoatGroup srcGroup, DamageBuff dmgBuff, bool isCritical) { }
+
+	public static int GetAuxiliaryGunDamage(BoatUnit srcBoat, BoatUnit targetUnit, float distanceCoe, DamageBuff dmgBuff) { }
+
+
+
+
